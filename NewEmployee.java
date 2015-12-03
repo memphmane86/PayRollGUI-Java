6e03831e75636a23363ba59fdf133f2a5bc7ee8a
memphmane86 <markelltrotter69@gmail.com>
2015-12-03 10:47:44 -0800

    Create NewEmployee.java

---
+++ NewEmployee.java
@@ -0,0 +1,29 @@
+package payrollgui;
+
+/**
+ *
+ * @author memphmane86
+ */
+class NewEmployee {
+
+  void setPayData(String empID, double hours, double rate) {
+  throw new UnsupportedOperationException("Not supported yet."); 
+ }
+
+ double computeGross() {
+ throw new UnsupportedOperationException("Not supported yet."); 
+}
+
+double computeNet(double gross) {
+  throw new UnsupportedOperationException("Not supported yet."); 
+}
+
+void setOvertimeRate(double overtimeRate) {
+  throw new UnsupportedOperationException("Not supported yet."); 
+}
+
+ void setInUnion(boolean selected) {
+  throw new UnsupportedOperationException("Not supported yet.");
+  }
+  
+}
